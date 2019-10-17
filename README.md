0500dcfb10c20e097d90c5ae9f57ed4a62dcd640
mbecerradev <martinbecerrarey@gmail.com>
2019-10-17 21:06:32 +0200

    Entorno de desarrollo LAMP con Redis y phpMyadmin

---
+++ README.md
@@ -0,0 +1,13 @@
+# LAMP + Redis
+
+* PHP 7.2
+* Apache 2.4
+* MySQL 8.0
+* phpMyAdmin
+* Redis
+
+## Instalar
+
+Clonar y  `docker-compose up -d`.
+
+Probar en `http://localhost`.
---
+++ www/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LAMP + Redis con phpmyadmin</title>
+    </head>
+    <body>
+        <h1>LAMP + Redis con phpmyadmin</h1>
+        <h2>Entorno:</h2>
+        <ul>
+            <li><?=apache_get_version()?></li>
+            <li>PHP <?=phpversion()?></li>
+            <li>
+            <?php
+            $link = mysqli_connect("mysql", "root", "tiger", null);
+
+            /* check connection */
+            if (mysqli_connect_errno()) {
+                printf("MySQL connecttion failed: %s", mysqli_connect_error());
+            } else {
+                /* print server version */
+                printf("MySQL Server %s", mysqli_get_server_info($link));
+            }
+            /* close connection */
+            mysqli_close($link);
+            ?>
+            </li>
+        </ul>
+        <h2>Enlaces:</h2>
+        <ul>
+            <li><a href="http://localhost/phpinfo.php">phpinfo()</a></li>
+            <li><a href="http://localhost:8080">phpMyAdmin</a></li>
+            <li><a href="http://localhost/test_db.php">Test DB Connection</a></li>
+        </ul>
+    </body>
+</html>
